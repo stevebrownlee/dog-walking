6c62b74cf2808fe896c27498f454273a5e871e6a
Steve Brownlee <chortlehoort@gmail.com>
2021-12-26 08:49:40 -0600

    Multiple cities

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,14 +1,14 @@
-import { getWalkers } from "./database.js"
+import { getCities } from "./database.js"
 
-const walkers = getWalkers()
+const cities = getCities()
 
 
 export const CityList = () => {
     let citiesHTML = "<ul>"
 
     // TODO: Alphabetize list of city name in ascending order
-    for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li>${city.name}</li>`
     }
 
     citiesHTML += "</ul>"
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,15 +1,64 @@
-import { getWalkers } from "./database.js"
+import { getCities, getWalkerCities, getWalkers } from "./database.js"
+
+const cities = getCities()
+const walkerCities = getWalkerCities()
+
+const filterWalkerCitiesByWalker = (walker) => {
+    // Define an empty array to store all of the assignment objects
+
+    // Iterate the array value of walkerCities
+
+    // Check if the primary key of the walker equals the foreign key on the assignment
+
+    // If it does, add the current object to the array of assignments
+
+    // After the loop is done, return the assignments array
+    const assignments = []
+
+    for (const assignment of walkerCities) {
+        if (assignment.walkerId === walker.id) {
+            assignments.push(assignment)
+        }
+    }
+
+    return assignments
+}
+
+const assignedCityNames = (assignments) => {
+    // Define an empty string that will get appended with matching cities
+
+    // Iterate the array of assignment objects
+
+    // For each assignment, iterate the cities array to find the match
+
+    // Add the name of the matching city to the array of city names
+    let cityNames = ""
+
+    for (const assignment of assignments) {
+        for (const city of cities) {
+            if (city.id === assignment.cityId) {
+                cityNames = `${cityNames} and ${city.name}`
+            }
+        }
+    }
+
+    return cityNames
+}
 
 document.addEventListener(
     "click",
     (clickEvent) => {
         const itemClicked = clickEvent.target
+
         if (itemClicked.id.startsWith("walker")) {
             const [,walkerId] = itemClicked.id.split("--")
 
             for (const walker of walkers) {
                 if (walker.id === parseInt(walkerId)) {
-                    window.alert(`${walker.name} services ${walker.city}`)
+                    const assignments = filterWalkerCitiesByWalker(walker)
+                    const cities = assignedCityNames(assignments)
+
+                    window.alert(`${walker.name} services ${cities}`)
                 }
             }
         }
